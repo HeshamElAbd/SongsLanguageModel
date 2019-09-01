5b91db70d063b759c0cb9caa3fa9b9a5e8d503b2
Hesham El Abd <30498915+HeshamElAbd@users.noreply.github.com>
2019-09-01 10:05:42 +0200

    Add files via upload

---
+++ CombineAllDatabasesIntoOne.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 31 10:54:08 2019
+@author: Hesham El Abd
+@Description:Combing all the database into a one DataBase ready for training 
+the model. 
+"""
+# load the modules: 
+import pickle
+
+with open("DataBase_one.pickle","rb") as input_:
+    dataBaseOne=pickle.load(input_)
+    
+with open("DataBase_Four.pickle","rb") as input_:
+    dataBaseFour=pickle.load(input_)
+
+LyricDataBase=dataBaseOne+"$"+dataBaseFour
+print("The size of the Final database is: "+str(len(LyricDataBase)/1e6)+
+      " Million chars")
+with open("Data/LyricsDataBase.pickle","wb") as output_:
+    pickle.dump(LyricDataBase,output_)
