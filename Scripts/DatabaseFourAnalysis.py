8b5bb199ad0e31534f14313c63fb8122e8b1062f
Hesham El Abd <30498915+HeshamElAbd@users.noreply.github.com>
2019-09-01 10:05:28 +0200

    Add files via upload

--- Scripts/DatabaseFourAnalysis.py
+++ Scripts/DatabaseFourAnalysis.py
@@ -7,19 +7,29 @@ Created on Sat Aug 31 10:44:42 2019
 @DataSource: https://www.kaggle.com/gyani95/380000-lyrics-from-metrolyrics
 """
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 import pickle
-import os
+import string
 ## Loading the DataBase: 
 data=pd.read_csv("Data/380000-lyrics-from-metrolyrics/lyrics.csv")
 data=data.dropna()
-# Average song length:
-songCorpa=""
-for song in data.lyrics.to_list():
-    songCorpa+=song+"$"
-##############################
+# Clean the database
 # save the DataBase 
+goodInputSong=True
+acceptableChars=string.ascii_lowercase +string.ascii_uppercase+string.punctuation+"\n"
+songCorpa=""
+songIndex=0
+for song in data.lyrics.to_list():
+    goodInputSong=True
+    for word in song.split(" "):
+        for char in word:
+            if (char not in acceptableChars):
+                goodInputSong=False
+                break
+    if goodInputSong:
+        songCorpa+=song+"$"
+    songIndex+=1
+    print("songs Processed: "+str(songIndex))
+
 with open("DataBase_Four.pickle","wb") as output_:
     pickle.dump(songCorpa,output_)
 
