7e8da30112609a67c9bb784d8298fe7cd66b6d21
Hesham El Abd <30498915+HeshamElAbd@users.noreply.github.com>
2019-09-01 09:51:25 +0200

    Add files via upload
    Cleaning the input text data

--- Scripts/DatabaseOneAnalysis.py
+++ Scripts/DatabaseOneAnalysis.py
@@ -4,13 +4,14 @@
 Created on Fri Aug 30 08:10:42 2019
 
 @author: Hesham El Abd
-@Description: Extract the song lyrics from the SongLyric dataBase 
+@Description: Extract the song lyrics from the SongLyric dataBase
 @DataSource: https://www.kaggle.com/mousehead/songlyrics
 """
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+import string 
 ## Navigating the database:
 data=pd.read_csv("Data/songlyrics/songdata.csv")
 print("Number of Artist: "+str(len(set(data.artist))))
@@ -36,15 +37,26 @@ plt.title("Average song length")
 plt.show()
 ########
 #Prepearing the first database: 
+goodInputSong=True
+acceptableChars=string.ascii_lowercase +string.ascii_uppercase+string.punctuation 
 songCorpa=""
+songIndex=0
 for song in data.text.to_list():
-    songCorpa+=song+"$"
+    goodInputSong=True
+    for words in song:
+        for char in words:
+            if (char not in acceptableChars):
+                goodInputSong=False
+        break;
+    if goodInputSong:
+        songCorpa+=song+"$"
+    songIndex+=1
+    print("songs Processed: "+str(songIndex))
 ##############################
 # save the DataBase 
 with open("DataBase_one.pickle","wb") as output_:
     pickle.dump(songCorpa,output_)
 
-    
 
 
 
